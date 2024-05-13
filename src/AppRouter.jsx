167a1f9d60d15421f35459c74b7e652a2918748d
vk880852 <115182567+vk880852@users.noreply.github.com>
2024-05-13 23:37:21 +0530

    Update AppRouter.jsx

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -8,26 +8,29 @@ import Dashboard from "./screens/Dashboard.jsx";
 const AppRouter = () => {
   return (
     <BrowserRouter>
-    <div className="container">
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-          <Link className="navbar-brand btn btn-primary" to="/">Hidden brand</Link>
-          <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
-            <li className="nav-item active">
-              <Link className="nav-link btn btn-primary" to="/">Home</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="btn btn-primary nav-link" to='/addproduct'>Add Product</Link>
-            </li>
-            <li className="nav-item">
-              <Link className="btn btn-primary nav-link" to="/dashboard">Dashboard</Link>
-            </li>
-          </ul>
-        </div>
-      </nav>
+      <div className="container">
+       <div className="container">
+  <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+      <Link className="navbar-brand btn btn-primary" to="/">Hidden brand</Link>
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item active">
+          <Link className="nav-link btn btn-primary" to="/">Home</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="btn btn-primary nav-link" to='/addproduct'>Add Product</Link>
+        </li>
+        <li className="nav-item">
+          <Link className="btn btn-primary nav-link" to="/dashboard">Dashboard</Link>
+        </li>
+      </ul>
+    </div>
+  </nav>
+</div>
+
       </div>
       <Routes>
         <Route path="/" element={<Home />} />
